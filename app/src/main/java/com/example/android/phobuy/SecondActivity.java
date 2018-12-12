5538ab34c61ac5f299c51c82c5dabd9db6a394c4
peteryu <jiming2@illinois.edu>
2018-12-11 20:31:51 -0600

    barcode finished

--- app/src/main/java/com/example/android/phobuy/SecondActivity.java
+++ app/src/main/java/com/example/android/phobuy/SecondActivity.java
@@ -1,24 +1,162 @@
 package com.example.android.phobuy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.Manifest.permission.CAMERA;
+
+
+public class SecondActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, ZXingScannerView.ResultHandler {
 
     private GestureDetector gestureDetector;
+
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView scannerView;
+    private static int camId = Camera.CameraInfo.CAMERA_FACING_BACK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+
+        scannerView = new ZXingScannerView(SecondActivity.this);
+        setContentView(scannerView);
 
         // for swipe right.
         gestureDetector = new GestureDetector(this);
+
+        int currentApiVersion = Build.VERSION.SDK_INT;
+
+        if(currentApiVersion >=  Build.VERSION_CODES.O_MR1) {
+            if (checkPermission()) {
+                Toast.makeText(getApplicationContext(), "Permission already granted!", Toast.LENGTH_LONG).show();
+            } else {
+                requestPermission();
+            }
+        }
     }
+
+    private boolean checkPermission()
+    {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission()
+    {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                if(scannerView == null) {
+                    scannerView = new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] per, int[] grantR) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantR.length > 0) {
+
+                    boolean cameraAccepted = grantR[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted){
+                        Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access camera", Toast.LENGTH_LONG).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and camera", Toast.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+                            if (shouldShowRequestPermissionRationale(CAMERA)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                                                    requestPermissions(new String[]{CAMERA},
+                                                            REQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.support.v7.app.AlertDialog.Builder(SecondActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        final String myResult = result.getText();
+        Log.d("QRCodeScanner", result.getText());
+        Log.d("QRCodeScanner", result.getBarcodeFormat().toString());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                scannerView.resumeCameraPreview(SecondActivity.this);
+            }
+        });
+        builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(myResult));
+                startActivity(browserIntent);
+            }
+        });
+        builder.setMessage(result.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+    }
+
+
+
     // helper function for swipe-right.
     @Override
     public boolean onDown(MotionEvent e) {
@@ -84,7 +222,7 @@ public class SecondActivity extends AppCompatActivity implements GestureDetector
 
     private void onSwipeRight() {
         Toast.makeText(this, "Swipe Left to Search", Toast.LENGTH_LONG).show();
-        Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+        Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
         startActivity(startIntent);
     }
 
@@ -96,4 +234,5 @@ public class SecondActivity extends AppCompatActivity implements GestureDetector
         gestureDetector.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
+
 }
